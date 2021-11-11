cca0d22a149aa66b85e5eeebcbb4ee3539ef061c
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-11 09:50:37 +0300

    Update 11_itc_cmp_str.cpp

--- 11_itc_cmp_str.cpp
+++ 11_itc_cmp_str.cpp
@@ -3,20 +3,28 @@
 string itc_cmp_str(string str1, string str2, int num)
 {
 	string str = "";
-	long long len = itc_len(str1);
+	long long len = itc_len(str1), len2 = itc_len(str2), j = -1;
 	if (num < 0) {
 		return "-1";
 	}
-	else if (num >= itc_len(str1)) {
+	else if (num >= len) {
 		return str1;
 	}
 	for (int i = 0; i < len; i++) {
 		if (i == num) {
 			str += str2;
-			i += itc_len(str2);
+			i += len2;
+			j = i;
+			break;
 		}
 		else {
 			str += str1[i];
+		}
+	}
+	if (j != -1) {
+		j -= len2;
+		for (j; j < len - len2; j++) {
+			str += str1[j];
 		}
 	}
 	return (str );
