646da8633dde89f634184dbe28528bf048d29c7c
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-11 10:50:08 +0300

    Update 9_itc_slice_str.cpp

--- 9_itc_slice_str.cpp
+++ 9_itc_slice_str.cpp
@@ -2,18 +2,16 @@
 
 string itc_slice_str(string str, int start, int end)
 {
-	if ((start < 0) || (end < 0)) {
-	return "-1";
-	}
-	if ((start > end) || (start >= itc_len(str))) {
-		return str;
-	}
-	if (end >= itc_len(str)) {
-		end = itc_len(str);
-	}
-	string sndstr = "";
-	for (int i = start; i <= end; i++) {
-		sndstr += str[i];
-	}
-	return sndstr;
+    long long len = itc_len(str);
+    if (start > end) {
+        return str;
+    }
+    string str2 = "";
+    if (end >= len) {
+        end = len - 1;
+    }
+    for (int i = start; i != end + 1; i++) {
+        str2 += str[i];
+    }
+    return str2;
 }
