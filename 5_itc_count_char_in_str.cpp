a810e09ae7f5c89de435e41f6332a2d13f561f9e
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-11 09:11:45 +0300

    Update 5_itc_count_char_in_str.cpp

--- 5_itc_count_char_in_str.cpp
+++ 5_itc_count_char_in_str.cpp
@@ -2,7 +2,8 @@
 
 int itc_count_char_in_str(char ch, string str)
 {
-	long long len = itc_len(str), nol = 0;
+	long long len = itc_len(str);
+	int nol = 0;
 	while (len > 0) {
 		if (str[len - 1] == ch) {
 			nol++;
